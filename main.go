7d5a23d91435a9fe22c75d6a2c21dfe820bdf6dd
Pius Arhanbhunde <pjacks419@gmail.com>
2022-01-23 23:26:53 +0100

    some little corrections to lexer

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ type Lexer struct {
 	token_list    []string
 }
 
-// linear search ~ constant time amortized
+// linear search ~ constant time amortized (I am dumb and might be wrong!)
 func (lex *Lexer) isIn(char, category string) bool {
 	for i := 0; i < len(category); i++ {
 		if char == string(category[i]) {
@@ -33,20 +33,26 @@ func (lex *Lexer) isIn(char, category string) bool {
 	return false
 }
 
-// tokenize the input
+// tokenize the input (very rough work)
 func (lex *Lexer) tokenize(input string) {
 	for lex.count < len(input) {
-		lex.removeComment(input)
-		lex.extractAlphanumerics(input)
-		lex.extractWhiteSpace(input)
-		lex.extractSymbol(input)
+		if lex.removeComment(input) {
+			continue
+		} else if lex.extractWhiteSpace(input) {
+			continue
+		} else if lex.extractAlphanumerics(input) {
+			continue
+		} else {
+			lex.extractSymbol(input)
+		}
+
 	}
 }
 
 // remove comment
-func (lex *Lexer) removeComment(input string) {
+func (lex *Lexer) removeComment(input string) bool {
 	if lex.count >= len(input) || lex.count+1 >= len(input) {
-		return
+		return false
 	}
 	// fmt.Println("Try... removeComment")
 	index := lex.count
@@ -56,17 +62,19 @@ func (lex *Lexer) removeComment(input string) {
 			if string(input[index]) != "\n" {
 				index += 1
 			} else {
+				lex.count = index
 				break
 			}
 		}
+		return true
 	}
-	lex.count = index
+	return false
 }
 
 // extract tokens
-func (lex *Lexer) extractAlphanumerics(input string) {
+func (lex *Lexer) extractAlphanumerics(input string) bool {
 	if lex.count >= len(input) {
-		return
+		return false
 	}
 	temp_h := ""
 	index := lex.count
@@ -80,14 +88,18 @@ func (lex *Lexer) extractAlphanumerics(input string) {
 		}
 	}
 	// fmt.Println(temp_h)
-	lex.count = index
-	// fmt.Println(lex.count)
+	if temp_h != "" {
+		lex.token_list = append(lex.token_list, temp_h)
+		lex.count = index
+		return true
+	}
+	return false
 }
 
 // extract whitespace they are also tokens but who cares...
-func (lex *Lexer) extractWhiteSpace(input string) {
+func (lex *Lexer) extractWhiteSpace(input string) bool {
 	if lex.count >= len(input) {
-		return
+		return false
 	}
 	index := lex.count
 	temp_h := ""
@@ -99,7 +111,12 @@ func (lex *Lexer) extractWhiteSpace(input string) {
 			break
 		}
 	}
-	lex.count = index
+	if temp_h != "" {
+		lex.token_list = append(lex.token_list, temp_h)
+		lex.count = index
+		return true
+	}
+	return false
 }
 
 func (lex *Lexer) extractSymbol(input string) {
@@ -115,7 +132,9 @@ func (lex *Lexer) extractSymbol(input string) {
 		!(string(input[index]) == "\t") {
 		temp_h += string(input[index])
 	}
-	temp_h = ""
+	if temp_h != "" {
+		lex.token_list = append(lex.token_list, temp_h)
+	}
 	index += 1
 	lex.count = index
 }
@@ -125,7 +144,10 @@ func main() {
 	var lexer_obj = Lexer{keywords: []string{"module", "grammar", "preference", "collaspe-w", "startsymbol"},
 		alphabet: "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-",
 		numerals: "0123456789"}
-	lexer_obj.tokenize("Expr.Add = [a + b]\n")
+	lexer_obj.tokenize("//Expr.Add = [a + b]\nExpr.Sub = [a - b]\n")
+	for i := 0; i < len(lexer_obj.token_list); i++ {
+		fmt.Println(lexer_obj.token_list[i])
+	}
 	// lexer_obj.removeComment("model lang\nimport lang2")
 	// lexer_obj.extractToken("module lang\nimport lang2")
 }
